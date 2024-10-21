7daeff4ac1926fa4db6f120ed7533ed1e4ed7af0
Feray Gulu-zada <fegulu@taltech.ee>
2024-10-21 18:17:40 +0300

    Part 1 FINALLY PASSES

--- lib/takso_web/controllers/booking_controller.ex
+++ lib/takso_web/controllers/booking_controller.ex
@@ -1,23 +1,22 @@
 defmodule TaksoWeb.BookingController do
   use TaksoWeb, :controller
 
+
   # Renders the form for a new booking
   def new(conn, _params) do
     render(conn, "new.html")
   end
 
-  # Creates a new booking
-  def create(conn, %{"booking" => booking_params}) do
-    # logic will be handled here
-    # For now just redirecting to the index page with a placeholder message.
-
-    conn
-    |> put_flash(:info, "Your taxi will arrive in 15 minutes.") # Placeholder message
-    |> redirect(to: "/bookings")
-  end
-
+  # Displays the list of bookings (index page)
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
+
+  def create(conn, _params) do
+    # Handle form submission logic
+    conn
+    |> put_flash(:info, "Your taxi will arrive in 10 minutes.")
+    |> redirect(to: "/bookings")
+  end
 end
